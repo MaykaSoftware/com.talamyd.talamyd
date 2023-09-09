a939f2060a16ca920b515bf80eac45564b61e677
Farid Benhaimoud <mayka.software@gmail.com>
2023-09-09 17:09:56 +0200

    Added courses implementation and adjusted authentication

--- src/main/kotlin/com/talamyd/course/CourseDaoImpl.kt
+++ src/main/kotlin/com/talamyd/course/CourseDaoImpl.kt
@@ -9,17 +9,17 @@ class CourseDaoImpl : CourseDao {
 }
 
 fun dummyListCourses(): List<TalamydCourse> {
-    val dummyList = mutableListOf<TalamydCourse>()
-    for (i in 1..10) {
-        dummyList.add(
-            TalamydCourse(
-                i,
-                "title $i",
-                "description $i",
-                "image url $i",
-                "course code $i"
-            )
-        )
-    }
+    val dummyList = mutableListOf<TalamydCourse>(
+        TalamydCourse(1, "Tawheed", "Learn fiqh the right way", "https://image.nl", "adc123"),
+        TalamydCourse(2, "Quraan", "Learn fiqh the right way", "https://image.nl", "adc123"),
+        TalamydCourse(3, "Fiqh", "Learn fiqh the right way", "https://image.nl", "adc123"),
+        TalamydCourse(4, "Hadith", "Learn fiqh the right way", "https://image.nl", "adc123"),
+        TalamydCourse(5, "Arabic", "Learn fiqh the right way", "https://image.nl", "adc123"),
+        TalamydCourse(6, "Tafsir", "Learn fiqh the right way", "https://image.nl", "adc123"),
+        TalamydCourse(7, "Tajweed", "Learn fiqh the right way", "https://image.nl", "adc123"),
+        TalamydCourse(8, "Akhlaaq", "Learn fiqh the right way", "https://image.nl", "adc123"),
+        TalamydCourse(9, "Anasheed", "Learn fiqh the right way", "https://image.nl", "adc123")
+    )
+
     return dummyList
 }
